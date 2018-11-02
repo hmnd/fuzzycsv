401fbe1c5de88d2dd645c99b4b3f3697abf80956
David Roizenman <david@hmnd.io>
2018-11-02 12:15:20 -0400

    Add package stuff

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fuzzycsv",
+    version="0.0.1",
+    author="David Roizenman",
+    author_email="david@hmnd.io",
+    description="Fuzzy matching for CSVs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hmnd/fuzzycsv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: Science/Engineering :: Information Analysis"
+    ],
+)
